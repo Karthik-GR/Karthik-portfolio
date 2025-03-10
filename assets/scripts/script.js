c9004c80fcdf2bb4bee6fdbe15f3ca0e01948098
Karthik-GR <karthikgr525@gmail.com>
2025-03-10 09:41:01 +0530

    first changes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,18 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const navbar = document.querySelector(".navbar");
   
-    // Function to handle scroll event
     function handleScroll() {
       if (window.scrollY > 200) {
-        navbar.classList.add("scrolled"); // Add 'scrolled' class when scrolled down
+        navbar.classList.add("scrolled"); 
       } else {
-        navbar.classList.remove("scrolled"); // Remove 'scrolled' class when at the top
+        navbar.classList.remove("scrolled"); 
       }
     }
   
-    // Attach the scroll event listener
     window.addEventListener("scroll", handleScroll);
   
-    // Call the function once to set the initial state
     handleScroll();
   });
